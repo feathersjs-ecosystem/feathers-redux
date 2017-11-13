864a938f994a4a890bb95f23794f86f2bd51474f
Bibek Subedi <bsubedi26@gmail.com>
2017-11-13 18:37:10 -0500

    added reducers for onCreated, onUpdated, onRemoved, onPatched

--- src/index.js
+++ src/index.js
@@ -210,6 +210,69 @@ const reduxifyService = (app, route, name = route, options = {}) => {
         reducerForServiceMethod(PATCH, false),
         reducerForServiceMethod(REMOVE, false),
 
+        { [ON_CREATED]: (state, action) => {
+          debug(`redux:${ON_CREATED}`, action);
+          const { data } = action.payload;
+          
+          return {
+            ...state,
+            [opts.queryResult]: Object.assign({}, state[opts.queryResult], {
+              data: state[opts.queryResult].data.concat(data),
+              total: state[opts.queryResult].total += 1
+            }),
+          };
+        } },
+
+        { [ON_UPDATED]: (state, action) => {
+          debug(`redux:${ON_UPDATED}`, action);
+          const { data } = action.payload;
+
+          return {
+            ...state,
+            [opts.queryResult]: Object.assign({}, state[opts.queryResult], {
+              data: state[opts.queryResult].data.map(item => {
+                if (item.id === data.id) {
+                  return data;
+                }
+                return item;
+              }),
+            }),
+          };
+        } },
+        
+        { [ON_PATCHED]: (state, action) => {
+          debug(`redux:${ON_PATCHED}`, action);
+          const { data } = action.payload;
+
+          return {
+            ...state,
+            [opts.queryResult]: Object.assign({}, state[opts.queryResult], {
+              data: state[opts.queryResult].data.map(item => {
+                if (item.id === data.id) {
+                  return data;
+                }
+                return item;
+              }),
+            }),
+          };
+        } },
+
+        { [ON_REMOVED]: (state, action) => {
+          debug(`redux:${ON_REMOVED}`, action);
+          const { data } = action.payload;
+          const removeIndex = queryResult.data.findIndex(item => item.id === data.id);
+
+          return {
+            ...state,
+            [opts.queryResult]: Object.assign({}, state[opts.queryResult], {
+              data: [
+                ...state[opts.queryResult].data.slice(0, removeIndex),
+                ...state[opts.queryResult].data.slice(removeIndex + 1)
+              ],
+            }),
+          };
+        } },
+
         // reset status if no promise is pending
         { [RESET]: (state, action) => {
           debug(`redux:${RESET}`, action);
